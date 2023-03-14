92f5d7f0522e94b1ef5809a83941ec072cf81917
limplaton <113909681+limplaton@users.noreply.github.com>
2023-03-14 09:01:00 +0800

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,9 +42,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://limmy.online/cable"
+  config.action_cable.url = "wss://178.128.232.207/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ "https://limmy.online" ]
+  config.action_cable.allowed_request_origins = [ "https://178.128.232.207" ]
   # config.hosts << "limmy.online"
   # config.hosts << "localhost"
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
@@ -93,5 +93,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_controller.default_url_options = { host: 'limmy.online' }
+  config.action_controller.default_url_options = { host: '178.128.232.207' }
 end
